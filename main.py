0830ba3046d48b7f9308efad45c2d0008a6588d4
Brian van der Bijl <peikos@peikos.net>
2022-09-22 11:52:44 +0200

    Commit in PyCharm

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Git')
+    print_hi('Git from PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
